67cd1432943ad8603514cdfed623532eb66e22f0
Thawitchai Boonsong <thawitchai285@gmail.com>
2025-01-06 13:56:14 +0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,8 +1,21 @@
-const express =  require('express')
+const express = require('express')
 const cors = require('cors')
-
+const { PrismaClient } = require('@prisma/client')
 const app = express()
+const prisma = new PrismaClient()
 
 app.use(express.json())
 app.use(cors())
 
+app.get('/electric', async (req, res) => {
+    const electric = await prisma.electric.findMany()
+    if (electric.length == 0) {
+        res.json({ message: "No data" })
+    } else {
+        res.json(electric)
+    }
+})
+
+app.listen(3000, () => {
+    console.log("PORT:3000")
+})
