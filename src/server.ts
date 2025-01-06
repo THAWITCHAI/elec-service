b3fbced163a303e95ef6ac2f2c404f713cf09bdc
Thawitchai Boonsong <thawitchai285@gmail.com>
2025-01-06 11:51:08 +0700

    update

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,8 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
+import {PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -9,13 +12,14 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
-app.get('/', (req: Request, res: Response) => {
-  res.send({ name: "Thawitchai" });
+app.get('/electric', async (req: Request, res: Response) => {
+    const electric = await prisma.electric.findMany()
+    res.json({electric});
 });
 
 // Start server
 app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
+    console.log(`Server is running at http://localhost:${PORT}`);
 });
 
 export = app;
