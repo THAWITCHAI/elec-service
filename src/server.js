e1aa6a9ed9a1b30cb627b8cbebc94eb7e1524bd4
Thawitchai Boonsong <thawitchai285@gmail.com>
2025-01-06 14:01:46 +0700

    ada

--- src/server.js
+++ src/server.js
@@ -1,19 +1,12 @@
 const express = require('express')
 const cors = require('cors')
-const { PrismaClient } = require('@prisma/client')
 const app = express()
-const prisma = new PrismaClient()
 
 app.use(express.json())
 app.use(cors())
 
 app.get('/electric', async (req, res) => {
-    const electric = await prisma.electric.findMany()
-    if (electric.length == 0) {
-        res.json({ message: "No data" })
-    } else {
-        res.json(electric)
-    }
+    res.send("Ok")
 })
 
 app.listen(3000, () => {
