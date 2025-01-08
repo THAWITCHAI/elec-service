8d1b58e09a3265a93b178c2fb7968647627c7d2f
Thawitchai Boonsong <thawitchai285@gmail.com>
2025-01-08 17:51:06 +0700

    update

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+const port = 3000;
+
+// Define a route
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello, Express!");
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
+
+export = app
