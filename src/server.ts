d2ed4c3ebfa9a6800aa8e93c377fa74dadc430e2
Thawitchai Boonsong <thawitchai285@gmail.com>
2025-01-09 09:59:42 +0700

    Prisma

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,26 @@
 import express, { Request, Response } from "express";
 const app = express();
-const port = 3000;
+const { PORT } = process.env;
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 // Define a route
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello, Express!");
+app.get("/electric", async (req: Request, res: Response) => {
+  try {
+    const electric = await prisma.electric.findMany()
+    res.json(electric);
+  } catch (error) {
+    console.log(error)
+    res.status(200).json({
+      message: 'Error'
+    })
+  }
 });
 
 // Start the server
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
 
 export = app
