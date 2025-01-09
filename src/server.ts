99d5c2c7322868e1a921e0d92b4be49044878a16
Thawitchai Boonsong <thawitchai285@gmail.com>
2025-01-09 11:10:39 +0700

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,15 @@
 import express, { Request, Response } from "express";
 const app = express();
 const PORT = process.env.PORT || 4000;
-// import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client'
 
-// const prisma = new PrismaClient()
+const prisma = new PrismaClient()
 
 // Define a route
 app.get("/electric", async (req: Request, res: Response) => {
   try {
-    // const electric = await prisma.electric.findMany()
-    res.json({name:'Thawitchai'});
+    const electric = await prisma.electric.findMany()
+    res.json(electric);
   } catch (error) {
     console.log(error)
     res.status(200).json({
